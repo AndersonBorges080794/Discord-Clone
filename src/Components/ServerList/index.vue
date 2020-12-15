29bacae323b827b0fe26bb7412625be50042a469
Anderson <andersonborges08@hotmail.com>
2020-12-15 15:46:13 -0300

    O video parou no minuto 40

---
+++ src/Components/ServerList/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+
+      SERVE LIST 
+
+
+  </div>
+</template>
+
+
+<script>
+
+</script>
+
+
+<style lang="scss" scoped>
+
+
+
+</style>
+
+
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+
+import Layout from './Components/Layout.vue'
+import './Styles/main.scss'
+
+new Vue({
+  el: '#app',
+  render: h => h(Layout)
+})
